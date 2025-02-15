0c6d203c2cc491a2dd8156b58fa14e335d68e171
vdhs248 <vdhs248@gmail.com>
2025-02-15 20:42:51 +0000

    conversion

---
+++ basices/conversion.js
@@ -0,0 +1,29 @@
+// let score = "3525"
+let score = null
+// console.log(typeof score)
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+/*
+"33" = > 33
+
+"444aaa" = > NaN
+ture => 1
+false = > 0
+
+
+
+*/
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
