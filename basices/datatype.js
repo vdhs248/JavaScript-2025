45ad9540c56aedc8a6c3f0e7f5071a2e2a17118c
vdhs248 <vdhs248@gmail.com>
2025-02-15 20:00:22 +0000

    datatype

---
+++ basices/datatype.js
@@ -0,0 +1,20 @@
+"use strict" // treat all js code newer version
+
+/*
+number => 2 to power 53
+
+bigint => 
+string = > " "
+boolean = ture /false
+null // standalone value / it represtation empty value
+undefined => 
+symbol => unique
+
+
+//
+
+object
+*/
+
+console.log(typeof "vinod");
+console.log(typeof null);
