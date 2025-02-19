68b3704e508ef855e3ed650062d52e5f02a1216d
vdhs248 <vdhs248@gmail.com>
2025-02-19 21:05:46 +0000

    done

---
+++ basices/dataType-summery.js
@@ -0,0 +1,54 @@
+// Primitive
+
+// 7 types 
+
+/*
+
+String
+Number
+Boolean
+null
+undefined
+symbol
+BigInt
+
+
+
+*/
+
+// Reference Type (Non primitive)
+
+// Array, Objects Functions
+
+const score = 100;
+const scoreValue = 100.23
+const loggedIn = false
+const outsideTem = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log( id === anotherId);
+
+const bigNumber = 2545454455445454555n
+
+
+const employeeName = ["Vinod ", "Kumar", "Kunal"];
+
+
+let obj ={
+   
+    name: "vinod",
+
+    email: "kumar@gmail.com",
+
+    city: "delhi"
+
+}
+
+const myfunction = function(){
+    console.log("Hello world");  
+}
+
+console.log(typeof bigNumber);
